0a1175ad13df571160b140cdb5ed6e4d0986ecb7
Andrew Janco <apjanco@gmail.com>
2022-01-15 14:06:15 -0500

    Update pretrain.py

--- newlang_project/scripts/pretrain.py
+++ newlang_project/scripts/pretrain.py
@@ -15,12 +15,12 @@ def pretrain(raw_text_folder:str, lang:str):
 
     lang = get_lang_class(lang)
     nlp = lang()
-
     #TODO handle only txt files? what of other formats
     texts = [t.read_text() for t in Path(raw_text_folder).iterdir() if t.suffix == ".txt"]
     docbin = DocBin()
     [docbin.add(nlp(text)) for text in texts]
-    docbin.to_disk(f'corpus/converted/raw_text.spacy')
+    save_path = Path.cwd() / 'corpus' / 'converted' / 'raw_text.spacy'
+    docbin.to_disk(save_path)
 
 
 if __name__ == "__main__":
